ad864ff596ca9c5fe35637306e1c473c23dca076
Enzo <enzo.pereira7899@gmail.com>
2025-02-14 17:38:59 +0100

    [feat](modal): add a display of modal element

---
+++ modal-app/src/Modal.tsx
@@ -0,0 +1,12 @@
+const Modal = () => {
+
+    return(
+        <div className="modal">
+            <h3>My modal</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos excepturi, est aperiam iure id dolor architecto eos beatae similique aut obcaecati corrupti! Quasi, itaque placeat. Dicta vitae distinctio voluptatum dolorum.</p>
+            <button>Ok</button>
+        </div>
+    );
+}
+
+export default Modal;
