f4fb645d82023c699fd58230978130ec51d5978a
Enzo <enzo.pereira7899@gmail.com>
2025-02-17 14:32:36 +0100

    [feat](Modal): add working buttons to show or hide modal

--- modal-app/src/App.tsx
+++ modal-app/src/App.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
 import Modal from "./Modal";
 
 function App() {
+
+  const [modalDisplay, setModalDisplay] = useState<string>("none");
+
+  const showModal = () => {
+    setModalDisplay("block");
+  }
   
   return(
     <>
       <h1>My Modal Project</h1>
-      <button className="show-modal-btn">Click me</button>
-      <Modal />
+      <button className="show-modal-btn" onClick={showModal}>Click me</button>
+      <Modal display={modalDisplay} setDisplay={setModalDisplay}/>
     </>
   );
 }
